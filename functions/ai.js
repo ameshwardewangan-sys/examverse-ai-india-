88fe9681f442e73faaa82cbc1435ac15ea9655d8
mr._ak__02 <294447718+ameshwardewangan-sys@users.noreply.github.com>
2026-06-27 21:29:03 +0530

    Export AI functions from ai.js

---
+++ functions/ai.js
@@ -0,0 +1,5 @@
+const ai = require("./ai");
+exports.askAI = ai.askAI;
+exports.generateQuiz = ai.generateQuiz;
+exports.evaluateAnswer = ai.evaluateAnswer;
+exports.generateSummary = ai.generateSummary;
